11b3e67a8661e9faa1403e68d5bbcdaf747f90ae
TonyFef <fefilov.toha1999@yandex.ru>
2021-11-27 22:47:50 +0300

    create: added swiper-slider in hotel session

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button-prev',
+      prevEl: '.slider-button-next',
+    },
+  });
