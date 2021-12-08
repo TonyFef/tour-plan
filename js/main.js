3b56ea8baccdafe6ee321f4657a6cab6b526e333
TonyFef <fefilov.toha1999@yandex.ru>
2021-12-08 11:58:00 +0300

    add parallax, edit validation forms

--- js/main.js
+++ js/main.js
@@ -9,11 +9,18 @@ $(document).ready(function () {
       prevEl: ".hotel-slider__button--prev",
     },
     effect: "flip",
+
+    // Пролистывание стрелками
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
   });
 
   const reviewsSwiper = new Swiper(".reviews-slider", {
     // Optional parameters
     loop: true,
+    effect: "flip",
 
     // Navigation arrows
     navigation: {
@@ -22,28 +29,70 @@ $(document).ready(function () {
     },
   });
 
-  var menuButton = $(".menu-button");
+  // Параллакс
+  $(".newslatter").parallax({
+    imageSrc: "img/newsletter-bg.jpeg",
+    speed: 0.4,
+  });
+
+  // Модальная кнопка
+  const menuButton = $(".menu-button");
+  // Вызов меню
   menuButton.on("click", function () {
-    console.log("Клик по кнопке меню");
     $(".navbar-bottom").toggleClass("navbar-bottom--visible");
   });
 
-  var modalButton = $('[data-toggle="modal"]');
-  var closeModalButton = $(".modal__close");
+  //Модальная кнопка
+  const modalButton = $('[data-toggle="modal"]');
+  const closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
 
+  // Вызов
   function openModal() {
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.addClass(".modal__overlay--visible");
-    modalDialog.addClass(".modal__dialog--visible");
+    const modalOverlay = $(".modal__overlay");
+    const modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
   }
+  // Закрытие
   function closeModal(event) {
     event.preventDefault();
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass(".modal__overlay--visible");
-    modalDialog.removeClass(".modal__dialog--visible");
+    const modalOverlay = $(".modal__overlay");
+    const modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
   }
+
+  //Обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "Invalid",
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+
+      messages: {
+        name: {
+          required: "Pleas specify youк name",
+          minlength: "Имя должно быть не короче 2 символов",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Телефон обязателен",
+        },
+      },
+    });
+  });
+  $(".phone").mask("+7 (999) 999-99-99");
 });
